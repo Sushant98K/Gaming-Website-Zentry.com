b69f7e56179eff2ebe8474a4802e6516fc7316e2
Sushant Kate <47480733+Sushant98K@users.noreply.github.com>
2025-03-01 14:49:20 +0530

    tailwind config done

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-orange-500 font-bold' >Welcome To Awwards</h1>
+      <h1 className="text-5xl text-blue-200 font-zentry">
+        Welcome To Awwards
+      </h1>
+      <p class="font-zentry text-violet-300">
+        This text uses the custom Zentry font and violet color.
+      </p>
     </main>
-  )
+  );
 }
 
 export default App
