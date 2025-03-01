619cc3422d36275d345566e0fa0d4051781150f3
Sushant Kate <47480733+Sushant98K@users.noreply.github.com>
2025-03-01 20:56:11 +0530

    Set Index.css for tailwindcss v4

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import React from 'react'
+import Hero from './components/Hero';
 
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl text-blue-200 font-zentry">
-        Welcome To Awwards
-      </h1>
-      <p class="font-zentry text-violet-300">
-        This text uses the custom Zentry font and violet color.
-      </p>
+    <main className='relative min-h-screen w-screen overflow-x-hidden' >
+      <Hero />
     </main>
   );
 }
