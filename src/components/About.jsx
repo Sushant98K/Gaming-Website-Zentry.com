576516bc2176deceeae9076613b5acd09577f94d
Sushant Kate <47480733+Sushant98K@users.noreply.github.com>
2025-03-03 01:35:58 +0530

    About Section Done

---
+++ src/components/About.jsx
@@ -0,0 +1,58 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+
+    useGSAP(() => {
+        const clipAnimation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#clip',
+                start: 'center center',
+                end: '+=800 center',
+                scrub: 0.5,
+                pin: true,
+                pinSpacing: true,
+            }
+        })
+        clipAnimation.to(".mask-clip-polly", {
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          width: "100vw",
+          height: "100vh",
+          borderRadius: 0,
+        });
+    })
+
+  return (
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <h2 className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Zentry
+        </h2>
+        <div className="mt-5 font-zentry text-center special-font text-4xl uppercase leading-[0.8] md:text-[6rem]">
+          Disc<b>o</b>ver The World's <br /> largest shared <b>a</b>dventure,
+        </div>
+        <div className="about-subtext font-circular-web">
+          <p>The Metagames begins-your life, now an epic MMORPG</p>
+          <p>
+            Zentry is the unified play layer that bridges players, agentic AI,
+            and <br /> blockchains, creating a new economic paradigm.
+          </p>
+        </div>
+      </div>
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-polly about-image">
+          <img
+            src="img/about.webp"
+            alt="background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
