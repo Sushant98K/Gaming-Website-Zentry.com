eff9510f9a22cd2a07b5af90b3bb0ee1eb9a4190
Sushant Kate <47480733+Sushant98K@users.noreply.github.com>
2025-03-04 14:57:16 +0530

    Features Start

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Features from "./components/Features";
 
 function App() {
   const [isAudioPlaying, setIsAudioPlaying] = useState(false);
@@ -47,6 +48,7 @@ function App() {
       />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 }
